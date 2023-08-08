8845e95fcdc96b881e0f7901822548bf578d0ea8
Rafael Eduardo Ortega Arrieta <rafael.ortega@bancofinandina.com>
2023-08-08 09:39:48 -0500

    Ajuste repetir - Numero de documento - Correo electronico - Numero celular

--- src/app/modules/create-runner/create-runner.component.ts
+++ src/app/modules/create-runner/create-runner.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { FormBuilder, FormGroup, SelectControlValueAccessor, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  SelectControlValueAccessor,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { OptionSelect } from 'src/app/data/interfaces/option-select.model';
 import { Runner } from 'src/app/data/interfaces/runner.model';
@@ -9,10 +16,9 @@ import { RunnerService } from 'src/app/data/services/runner.service';
 @Component({
   selector: 'app-create-runner',
   templateUrl: './create-runner.component.html',
-  styleUrls: ['./create-runner.component.scss']
+  styleUrls: ['./create-runner.component.scss'],
 })
-export class CreateRunnerComponent implements OnInit{
-
+export class CreateRunnerComponent implements OnInit {
   private readonly _formBuilder = inject(FormBuilder);
   private _runnerService = inject(RunnerService);
   private _parametryService = inject(ParametryService);
@@ -36,43 +42,65 @@ export class CreateRunnerComponent implements OnInit{
     return this._formBuilder.group({
       firstName: ['', [Validators.required]],
       lastName: ['', [Validators.required]],
-      email: ['', [Validators.required, Validators.email]],
-      phone: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.email,
+          this.handleValidateRepeat('repeatEmail'),
+        ],
+      ],
+      repeatEmail: [
+        '',
+        [
+          Validators.required,
+          Validators.email,
+          this.handleValidateRepeat('email'),
+        ],
+      ],
+      phone: ['', [Validators.required, Validators.pattern('^[0-9]+$'), this.handleValidateRepeat('repeatPhone')]],
+      repeatPhone: ['', [Validators.required, Validators.pattern('^[0-9]+$'), this.handleValidateRepeat('phone')]],
       birthDate: ['', [Validators.required]],
       codeDocumentType: ['CC', [Validators.required]],
       //documentNumber: ['79856908', [Validators.required]],
-      documentNumber: ['', [Validators.required, Validators.pattern(/^[A-Za-z0-9]+$/)]],
+      documentNumber: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(/^[A-Za-z0-9]+$/),
+          this.handleValidateRepeat('repeatDocumentNumber'),
+        ],
+      ],
+      repeatDocumentNumber: [
+        '',
+        [Validators.required, this.handleValidateRepeat('documentNumber')],
+      ],
       codeCity: ['', [Validators.required]],
       address: ['', [Validators.required]],
       bloodType: ['', [Validators.required]],
       codeCountryNationality: ['', [Validators.required]],
       idGender: ['', [Validators.required]],
       emergencyContactName: ['', [Validators.required]],
-      emergencyContactPhone: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+      emergencyContactPhone: [
+        '',
+        [Validators.required, Validators.pattern('^[0-9]+$')],
+      ],
     });
   }
 
   initSelects() {
-    this._parametryService
-      .getParametryDocumentTypes()
-      .subscribe((data) => {
-        this.documentTypes = data as OptionSelect[];
-      });
-    this._parametryService
-      .getParametryCities()
-      .subscribe((data) => {
-        this.cities = data as OptionSelect[];
-      });
-    this._parametryService
-      .getParametryCountries()
-      .subscribe((data) => {
-        this.countries = data as OptionSelect[];
-      });
-    this._parametryService
-      .getParametryGenders()
-      .subscribe((data) => {
-        this.genders = data as OptionSelect[];
-      });
+    this._parametryService.getParametryDocumentTypes().subscribe((data) => {
+      this.documentTypes = data as OptionSelect[];
+    });
+    this._parametryService.getParametryCities().subscribe((data) => {
+      this.cities = data as OptionSelect[];
+    });
+    this._parametryService.getParametryCountries().subscribe((data) => {
+      this.countries = data as OptionSelect[];
+    });
+    this._parametryService.getParametryGenders().subscribe((data) => {
+      this.genders = data as OptionSelect[];
+    });
   }
 
   changeStep(step: number) {
@@ -86,19 +114,20 @@ export class CreateRunnerComponent implements OnInit{
           this.runnerForm.get('documentNumber') &&
           this.runnerForm.get('codeDocumentType')
         ) {
-          this._runnerService.searchRunner(
-            this.runnerForm.get('documentNumber')?.value,
-            this.runnerForm.get('codeDocumentType')?.value
-          ).subscribe(response => {
-            if(response.data != null){
-              this.runner = response.data;
-              this.goToRunnerData();
-            }
-            else{
-              this.progress = 25;
-              this.step = 1;
-            }
-          });
+          this._runnerService
+            .searchRunner(
+              this.runnerForm.get('documentNumber')?.value,
+              this.runnerForm.get('codeDocumentType')?.value
+            )
+            .subscribe((response) => {
+              if (response.data != null) {
+                this.runner = response.data;
+                this.goToRunnerData();
+              } else {
+                this.progress = 25;
+                this.step = 1;
+              }
+            });
         }
         break;
       case 2:
@@ -109,50 +138,78 @@ export class CreateRunnerComponent implements OnInit{
   }
 
   onSubmit(): void {
-    if (this.runnerForm.valid){
+    if (this.runnerForm.valid) {
       this.progress = 75;
-      this._runnerService.createRunner(this.runnerForm.value).subscribe((response) => {
-        if (response.code == 0) {
-          this.runner = response.data;
-          alert(response.message);
-          this.goToRunnerData();
-        }
-      });
+      this._runnerService
+        .createRunner(this.runnerForm.value)
+        .subscribe((response) => {
+          if (response.code == 0) {
+            this.runner = response.data;
+            alert(response.message);
+            this.goToRunnerData();
+          }
+        });
     }
   }
 
-  goToRunnerData(){
+  goToRunnerData() {
     this._runnerService.setLocalRunner$(this.runner);
     this._router.navigate(['runnerData']);
   }
 
-
   //#region Validators
   getValidation(name: string) {
-    return this.runnerForm.get(name)
+    return this.runnerForm.get(name);
   }
 
   handleInvalidOne(): boolean {
-    return this.getValidation('codeDocumentType')?.invalid as boolean
-    || this.getValidation('documentNumber')?.invalid as boolean
+    return (
+      (this.getValidation('codeDocumentType')?.invalid as boolean) ||
+      (this.getValidation('documentNumber')?.invalid as boolean) ||
+      (this.getValidation('repeatDocumentNumber')?.invalid as boolean)
+    );
   }
 
   handleInvalidSecond(): boolean {
-    return this.getValidation('firstName')?.invalid as boolean
-    || this.getValidation('lastName')?.invalid as boolean
-    || this.getValidation('bloodType')?.invalid as boolean
-    || this.getValidation('email')?.invalid as boolean
-    || this.getValidation('phone')?.invalid as boolean
-    || this.getValidation('birthDate')?.invalid as boolean
+    return (
+      (this.getValidation('firstName')?.invalid as boolean) ||
+      (this.getValidation('lastName')?.invalid as boolean) ||
+      (this.getValidation('bloodType')?.invalid as boolean) ||
+      (this.getValidation('email')?.invalid as boolean) ||
+      (this.getValidation('phone')?.invalid as boolean) ||
+      (this.getValidation('repeatEmail')?.invalid as boolean) ||
+      (this.getValidation('repeatPhone')?.invalid as boolean) ||
+      (this.getValidation('birthDate')?.invalid as boolean)
+    );
   }
 
   handleInvalidThird(): boolean {
-    return this.getValidation('codeCity')?.invalid as boolean
-    || this.getValidation('address')?.invalid as boolean
-    || this.getValidation('codeCountryNationality')?.invalid as boolean
-    || this.getValidation('idGender')?.invalid as boolean
-    || this.getValidation('emergencyContactName')?.invalid as boolean
-    || this.getValidation('emergencyContactPhone')?.invalid as boolean
+    return (
+      (this.getValidation('codeCity')?.invalid as boolean) ||
+      (this.getValidation('address')?.invalid as boolean) ||
+      (this.getValidation('codeCountryNationality')?.invalid as boolean) ||
+      (this.getValidation('idGender')?.invalid as boolean) ||
+      (this.getValidation('emergencyContactName')?.invalid as boolean) ||
+      (this.getValidation('emergencyContactPhone')?.invalid as boolean)
+    );
+  }
+
+  handleValidateRepeat(name: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      const controlValue = <string>control.value;
+
+      if (!controlValue) return null;
+
+      const controlRepeat = this.runnerForm.get(name)?.value;
+
+      if (!controlRepeat) return null;
+
+      if (controlValue != controlRepeat) {
+        return { repeatValidation: true };
+      }
+
+      return null;
+    };
   }
 
   //#endregion
